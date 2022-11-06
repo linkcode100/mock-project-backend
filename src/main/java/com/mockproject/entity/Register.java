f60f4f83761a037f656548aa2fc8931723ef37ae
Akanksha Dhage <akankshadhage30072001@gmail.com>
2022-11-06 14:07:02 +0530

    entity class addeed

--- src/main/java/com/mockproject/entity/Register.java
+++ src/main/java/com/mockproject/entity/Register.java
@@ -1,5 +1,94 @@
 package com.mockproject.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="User")
 public class Register {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "firstName")
+	private String firstName;
+	
+	@Column(name = "lastName")
+	private String lastName;
+	
+	@Column(name = "emailId")
+	private String emailId;
+	
+	@Column(name = "password")
+	private String password;
+	
+	
+	public Register() {
+		
+	}
+
+	public Register(int id, String firstName, String lastName, String emailId, String password) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 }
--- src/main/java/com/mockproject/services/RegisterService.java
+++ src/main/java/com/mockproject/services/RegisterService.java
@@ -1,5 +1,8 @@
 package com.mockproject.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegisterService {
 
 }
